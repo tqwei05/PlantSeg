9309c4952edcaab679c26dbca41c0fe48e055bb6
tqwei05 <167828007+tqwei05@users.noreply.github.com>
2024-10-06 13:54:01 +1000

    Add files via upload

---
+++ configs/deeplabv3plus/deeplabv3plus_r50-160k_docseg_binary-128x128.py
@@ -0,0 +1,9 @@
+_base_ = [
+    '../_base_/models/deeplabv3plus_r50-d8.py', '../_base_/datasets/docseg_binary.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
+]
+crop_size = (512, 512)
+data_preprocessor = dict(size=crop_size)
+model = dict(
+    data_preprocessor=data_preprocessor,
+    test_cfg=dict(crop_size=(512, 512), stride=(85, 85)))
